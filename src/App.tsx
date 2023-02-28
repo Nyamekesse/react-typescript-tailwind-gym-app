75c88523a2c273eefc8ab40e3bff1900017037bf
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-02-28 19:02:00 +0000

    added and completed Home page. Added all effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "./scenes/Navbar";
 import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/Home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -27,6 +28,7 @@ function App() {
         setSelectedPage={setSelectedPage}
         isTopOfPage={isTopOfPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
